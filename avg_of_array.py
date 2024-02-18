07c8c7b5ec2af66f1cb6574d7ea454bd3a523d6d
ankurjava12 <ankurjava12@gmail.com>
2024-02-18 22:55:32 +0530

    add some programms

--- avg_of_array.py
+++ avg_of_array.py
@@ -1,8 +1,39 @@
-n = int(input("enter a number : "))
-arr = []
-for i in range(n):
-    element = int(input("enter the arry element : "))
-    arr.append(element)
-    avg = sum(arr)/len(arr)
-print(arr)
-print("Average of the array element",avg)
+# n = int(input("enter a number : "))
+# arr = []
+# for i in range(n):
+#     element = int(input("enter the arry element : "))
+#     arr.append(element)
+#     avg = sum(arr)/len(arr)
+# print(arr)
+# print("Average of the array element",avg)
+
+
+# 
+# def avg_arr(len_arr):
+#     arr=[]
+#     for i in range(len_arr):
+#         element=int(input("Enter a array element : "))
+#         arr.append(element)
+#     avg = sum(arr)/len(arr)
+#     print(f"The avrage of array element is : {avg}")
+
+
+
+# len_arr = int(input("Enter the length of array: "))
+
+# avg_arr(len_arr)
+
+def create_arr(len_arr):
+    arr=[]
+    for i in range(len_arr):
+        element=int(input("Enter a array element : "))
+        arr.append(element)
+    
+def avg_arr(arr):
+    for i in range(len_arr):
+        avg = sum(arr)/len(arr)
+    print(f"The avrage of array element is : {avg}")
+
+len_arr = int(input("Enter the len of array : "))
+arr = create_arr(len_arr)
+avg_arr(arr)
---
+++ matrix.py
@@ -0,0 +1,51 @@
+# mat = [
+#     [1,2,3],
+#     [2,3,4],
+#     [3,4,5]
+# ]
+# for i in mat:
+#     for j in i:
+#         print(j, end=" ")
+#     print()
+
+
+# Using function--
+# def print_matrix(mat):
+#     for i in mat:
+#         for j in i:
+#             print(j,end=" ")
+#         print()
+
+# mat = [
+#     [1,1,1],
+#     [2,2,2],
+#     [3,3,3]
+# ]
+
+# print_matrix(mat)
+
+
+# Enter matrix by user input--
+def create_matrix(m,n):
+    mat = []
+    for i in range(m):
+        row =[]
+        for j in range(n):
+            input_num = int(input(f"enter the value of mat[{i}][{j}] : "))
+            row.append(input_num)
+        mat.append(row)
+    return mat
+
+def show(mat):
+    for i in mat:
+        for j in i:
+            print(j, end=" ")
+        print()
+
+rows = int(input("Enter rows : "))
+cols = int(input("Enter cols : "))
+
+# create_matrix(rows,cols)
+show(create_matrix(rows,cols))
+
+
